54c309476e9353e87ff58917bdc3dffcc3cf95a7
Alban Gossard <alban.paul.gossard@gmail.com>
2026-04-15 20:38:42 +0200

    align tests with ODINN to allow execution with include

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,21 @@
+import Pkg
+function is_included_in_repl()
+    for frame in StackTraces.stacktrace()
+        if occursin("start_repl_backend", string(frame.func))
+            return true
+        end
+    end
+    return false
+end
+
+Pkg.activate(dirname(Base.current_project()))
+Pkg.instantiate() # Need this to setup the ODINN env for multiprocessing
+if is_included_in_repl()
+    # The Project.toml of the test environment to be used when running with include is in a subfolder to avoid that Julia uses this file in test mode
+    Pkg.activate(dirname(Base.current_project())*"/test/test_env/")
+    Pkg.resolve()
+end
+
 using Revise
 using MassBalanceMachine
 using Test
